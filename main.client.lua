732bde3d85f4da75096a518c23f408de505f5e90
Emplic <89635087+Emplic@users.noreply.github.com>
2026-07-18 00:28:43 +0300

    Update main.client.lua

--- main.client.lua
+++ main.client.lua
@@ -25,7 +25,7 @@ do
 			WindUI = require(cloneref(ReplicatedStorage:WaitForChild("WindUI"):WaitForChild("Init")))
 		else
 			WindUI =
-				loadstring(game:HttpGet("https://raw.githubusercontent.com/Emplic/WindU/loader.lua"))()
+				loadstring(game:HttpGet("https://raw.githubusercontent.com/Emplic/WindU/refs/heads/main/loader.lua"))()
 		end
 	end
 end
